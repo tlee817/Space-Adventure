b66652aa42ea6a178c3229a1a45076ba4947523b
Christopher Gharghou <christophergargour@gmail.com>
2025-02-24 17:57:55 -0800

    Rocket and cube

--- src/scene3.js
+++ src/scene3.js
@@ -1,3 +1,42 @@
+// import * as THREE from 'three';
+
+export let spaceship2;
+export let boosterEngine;
+export let stabilityWings;
+
+// export function createScene3() {
+//     const scene = new THREE.Scene();
+
+//     spaceship2 = new THREE.Mesh( new THREE.ConeGeometry(1, 2, 32), new THREE.MeshBasicMaterial({ color: 0xffff00 }));
+//     spaceship2.position.set(0, 0, 0);
+
+//     boosterEngine = new THREE.Mesh( new THREE.CylinderGeometry(0.8, 0.8, 3), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+//     boosterEngine.position.set(0, -2.2, 0);
+
+//     const wingStabilityVertices = new Float32Array([
+//         0,  1, 0, // Top
+//        -1, -4, 0, // lower left
+//         1, -4, 0  // lower right
+//     ]);
+      
+//     // BufferGeometry sets up wingStability from the vertices
+//     const wingsGeoBuffer = new THREE.BufferGeometry();
+//     wingsGeoBuffer.setAttribute('position', new THREE.BufferAttribute(wingStabilityVertices, 3));
+
+//     stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }));
+
+    // scene.add(spaceship2);
+    // scene.add(boosterEngine);
+    // scene.add(stabilityWings);
+
+//     // need light (currently from scene1)
+//     let sunLight = new THREE.PointLight(0xffffff, 1, 0, 1);
+//     sunLight.position.set(0, 3, 0);
+//     scene.add(sunLight);
+
+//     return scene;
+// }
+
 import * as THREE from 'three';
 
 
@@ -13,8 +52,27 @@ export function createScene3() {
   const spaceship = new THREE.Mesh(boxGeometry, boxMaterial);
   spaceship.position.set(-5, 0, 0);
   scene.add(spaceship);
+////////////
+    spaceship2 = new THREE.Mesh( new THREE.ConeGeometry(1, 2, 32), new THREE.MeshBasicMaterial({ color: 0xffff00 }));
+    spaceship2.position.set(-8, 0, 0);
 
+    boosterEngine = new THREE.Mesh( new THREE.CylinderGeometry(0.8, 0.8, 3), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+    boosterEngine.position.set(-8, -2.2, 0);
 
+        const wingStabilityVertices = new Float32Array([
+        -8,  1, 0, // Top
+       -9, -4, 0, // lower left
+        -7, -4, 0  // lower right
+    ]);
+
+        const wingsGeoBuffer = new THREE.BufferGeometry();
+    wingsGeoBuffer.setAttribute('position', new THREE.BufferAttribute(wingStabilityVertices, 3));
+
+    stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }));
+        scene.add(spaceship2);
+    scene.add(boosterEngine);
+    scene.add(stabilityWings);
+/////////
   window.addEventListener("keydown", function(event) {
    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) {
      event.preventDefault();
