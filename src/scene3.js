3553387dda4a0942ef4eba99eb7275f8f56fb98c
SnowFlakeStudyBee <snowflakestudybee@gmail.com>
2025-03-04 21:11:53 -0800

    scene3 updates in progress
    Added wraps. Working on the flame now.

--- src/scene3.js
+++ src/scene3.js
@@ -1,8 +1,11 @@
 // import * as THREE from 'three';
 
-export let spaceship2;
-export let boosterEngine;
-export let stabilityWings;
+//export 
+let noseCone;
+//export 
+let boosterEngine;
+//export 
+let stabilityWings;
 
 // export function createScene3() {
 //     const scene = new THREE.Scene();
@@ -40,39 +43,118 @@ export let stabilityWings;
 import * as THREE from 'three';
 
 
+
 export function createScene3() {
   const scene = new THREE.Scene();
    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-   // Create a box (spaceship)
+
+
+
+  // Create a box (spaceship)
   const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
   const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
   const spaceship = new THREE.Mesh(boxGeometry, boxMaterial);
   spaceship.position.set(-5, 0, 0);
   scene.add(spaceship);
 ////////////
-    spaceship2 = new THREE.Mesh( new THREE.ConeGeometry(1, 2, 32), new THREE.MeshBasicMaterial({ color: 0xffff00 }));
-    spaceship2.position.set(-8, 0, 0);
 
-    boosterEngine = new THREE.Mesh( new THREE.CylinderGeometry(0.8, 0.8, 3), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+
+
+
+
+const textures = ['../assets/FuturisticCoating1.jpg', '../assets/FuturisticCoating2.jpg', '../assets/FuturisticCoating3.jpg', 
+  '../assets/FuturisticCoating4.jpg', '../assets/FuturisticCoating5.jpg'];
+  
+const flametextures = ['../assets/flame1.jpg', '../assets/flame2.webp', '../assets/flame3.svg',
+  '../assets/flame4.jpg', '../assets/flame5.jpg', '../assets/flame6.webp', '../assets/flame7.jpg',
+  '../assets/flame8.avif', '../assets/flame9.jpeg'];
+
+
+  //     for (let i = 0; i < 7; i++) {
+  //         const planettexture = new THREE.TextureLoader().load(textures[i]);
+  //         planettexture.wrapS = THREE.RepeatWrapping;
+  //         planettexture.wrapT = THREE.RepeatWrapping;
+  //         const sphere = new THREE.Mesh(
+  //             new THREE.SphereGeometry(planetRadii[i], 64, 64),
+  //             new THREE.MeshStandardMaterial({map: planettexture, roughness: 1.0, metalness: 0.2})
+  //         );
+  //         sphere.userData = {
+  //             angle: Math.random() * Math.PI * 2,
+  //             radius: orbitRadii[i],
+  //             speed: orbitSpeeds[i]
+  //         };
+  //         scene.add(sphere);
+  //         spheres.push(sphere);
+          
+  //     }
+
+
+
+  const spaceShipTexture = new THREE.TextureLoader().load(textures[2]);
+  spaceShipTexture.wrapS = THREE.RepeatWrapping;
+  spaceShipTexture.wrapT = THREE.RepeatWrapping;
+
+
+    noseCone = new THREE.Mesh( new THREE.ConeGeometry(1, 2, 32), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+    //noseCone.position.set(-8, 0, 0);
+    noseCone.position.set(0, 2.2, 0);
+
+
+    boosterEngine = new THREE.Mesh( new THREE.CylinderGeometry(0.8, 0.8, 3), new THREE.MeshBasicMaterial({ color: 0xffffff }));
     boosterEngine.position.set(-8, -2.2, 0);
+    //boosterEngine.position.set(0, 0, 0);
 
-        const wingStabilityVertices = new Float32Array([
-        -8,  1, 0, // Top
-       -9, -4, 0, // lower left
-        -7, -4, 0  // lower right
-    ]);
 
-        const wingsGeoBuffer = new THREE.BufferGeometry();
+    // const wingStabilityVertices = new Float32Array([
+    //    -8,  1, 0, // Top
+    //    -9, -4, 0, // lower left
+    //    -7, -4, 0  // lower right
+    // ]);
+
+  //   const wingStabilityVertices = new Float32Array([
+  //     -8,  1, 0, // Top
+  //     -9.5, -3.7, 0, // lower left
+  //     -6.5, -3.7, 0  // lower right
+  //  ]);
+
+
+   const wingStabilityVertices = new Float32Array([
+    0,  3.2, 0, // Top
+    -1.5, -1.5, 0, // lower left
+    1.5, -1.5, 0  // lower right
+ ]);
+
+    const wingsGeoBuffer = new THREE.BufferGeometry();
     wingsGeoBuffer.setAttribute('position', new THREE.BufferAttribute(wingStabilityVertices, 3));
+    stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ color: 0x4293f5, side: THREE.DoubleSide }));
 
-    stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }));
-        scene.add(spaceship2);
-    scene.add(boosterEngine);
+    // assemble the spaceship...
+    //boosterEngine.position.set(-15, 0, 0);
+    
+    boosterEngine.add(noseCone);
     scene.add(stabilityWings);
+
+
+    scene.add(boosterEngine);
+
+    //boosterEngine.setAttribute('material', new THREE.MeshStandardMaterial({map: spaceShipTexture, roughness: 1.0, metalness: 0.2}));
+    boosterEngine.material.map = spaceShipTexture;
+    noseCone.material.map = spaceShipTexture;
+    stabilityWings.material.map = spaceShipTexture;
+
+    boosterEngine.add(stabilityWings);
+
+
+    // scene.add(noseCone);
+    // scene.add(boosterEngine);
+    // scene.add(stabilityWings);
 /////////
+
+
+
   window.addEventListener("keydown", function(event) {
    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) {
      event.preventDefault();
@@ -137,3 +219,78 @@ export function createScene3() {
    return scene;
 }
 
+
+
+
+
+// import * as THREE from 'three';
+// // import planetImg from '../assets/planettexture.png';
+
+// export function createScene2(renderer, camera) {
+//     const scene = new THREE.Scene();
+
+//     const pointLight = new THREE.PointLight(0xFFFFFF, 5, 25, 0.5);
+//     pointLight.position.set(0, 0, 0);
+//     scene.add(pointLight);
+//     const ambientLight = new THREE.AmbientLight(0xffffff, 0.02);
+//     scene.add(ambientLight);
+
+    
+//     const spheres = [];
+//     const orbitSpeeds = [5/4, 5/5, 5/7.5, 5/10, 5/12.5, 5/15, 5/17.5];
+//     const orbitRadii = [2, 4, 6, 8, 11, 14, 19];
+//     const planetRadii = [1, 1.5, 1.5, 2, 2, 2.5, 3]
+//     const textures = ['../assets/planettexture.png', '../assets/orange.png', '../assets/red.jpg', '../assets/blue_orange.png',
+//     '../assets/emerald.png', '../assets/water.jpg', '../assets/earth.png'];
+
+//     for (let i = 0; i < 7; i++) {
+//         const planettexture = new THREE.TextureLoader().load(textures[i]);
+//         planettexture.wrapS = THREE.RepeatWrapping;
+//         planettexture.wrapT = THREE.RepeatWrapping;
+//         const sphere = new THREE.Mesh(
+//             new THREE.SphereGeometry(planetRadii[i], 64, 64),
+//             new THREE.MeshStandardMaterial({map: planettexture, roughness: 1.0, metalness: 0.2})
+//         );
+//         sphere.userData = {
+//             angle: Math.random() * Math.PI * 2,
+//             radius: orbitRadii[i],
+//             speed: orbitSpeeds[i]
+//         };
+//         scene.add(sphere);
+//         spheres.push(sphere);
+        
+//     }
+
+
+//     spheres[0].position.y=2.5;
+//     spheres[1].position.x=-4;
+//     spheres[2].position.y=-4;
+//     spheres[3].position.z=-4;
+//     spheres[4].position.x=6;
+//     spheres[5].position.x=-4;
+//     spheres[5].position.y=-4;
+//     spheres[6].position.y=6;
+//     spheres[6].position.z=-7;
+
+//     // Set Planets' Hitpoints
+//     for (let i = 0; i < 7; i++) 
+//     {
+//        spheres[i].userData.hitpoint = i+1;
+//     }
+//     const clock = new THREE.Clock();
+//     function animate() {
+//         requestAnimationFrame(animate);
+//         renderer.render(scene, camera);
+//         spheres.forEach(sphere => {
+//             sphere.userData.angle += sphere.userData.speed * 0.03;
+//             sphere.position.x = sphere.userData.radius * Math.cos(sphere.userData.angle);
+//             sphere.position.z = sphere.userData.radius * Math.sin(sphere.userData.angle);
+//         });
+        
+//     }
+
+
+//     animate();
+
+//     return scene;
+// }
