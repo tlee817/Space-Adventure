a7f8da61415a42588cf1c457e185f9891cc03691
Christopher Gharghou <christophergargour@gmail.com>
2025-03-08 12:46:58 -0800

    Added stars to planet shooting game

--- src/main.js
+++ src/main.js
@@ -56,6 +56,7 @@ window.addEventListener('click', (event) => {
     }
     }else if(activeScene===scene2)  // Planet Shooting
     {
+
         const intersects = raycaster.intersectObjects(scene2.children);
         if (intersects.length > 0) 
         {
