aad8c8be43a022b4512992bf9ceb4c526b8bd77b
LeungAshlee <149737088+LeungAshlee@users.noreply.github.com>
2025-03-13 17:13:39 -0700

    Update scene2Pt5.js
    - Fixed Collision detection
    - Start when space bar is pressed

--- src/scene2Pt5.js
+++ src/scene2Pt5.js
@@ -1,22 +1,24 @@
 import * as THREE from 'three';
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-//import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
 export function createScene2Pt5(renderer, camera) {
-    const pipeZPosition = -20;
+    const pipeZPosition = -18.1;
+    const listener = new THREE.AudioListener();
+    camera.add(listener); 
+    const backgroundMusic = new THREE.Audio(listener);
+    
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load('assets/StarWars.mp3', function(buffer) {
+        backgroundMusic.setBuffer(buffer);
+        backgroundMusic.setLoop(true); 
+        backgroundMusic.setVolume(0.1); 
+        backgroundMusic.play();
+    });
 
   const scene = new THREE.Scene();
-  camera.position.set(0, 7, 30);
-  //camera.
-   //let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-   //const renderer = new THREE.WebGLRenderer();
-   //renderer.setSize(window.innerWidth, window.innerHeight);
-   //document.body.appendChild(renderer.domElement);
-//    const controls = new OrbitControls(camera, renderer.domElement);
-//    controls.maxDistance = 100;
-//    controls.minDistance = 100;
+  camera.position.set(0, 15, 35);
+  camera.lookAt(new THREE.Vector3(0, 0, -5));
   
    // Add lighting
   const directionalLight = new THREE.DirectionalLight(0xffffd6, 10);
@@ -34,7 +36,18 @@ export function createScene2Pt5(renderer, camera) {
   scene.add(directionalLight2);
 
   let gameComplete = false;
-
+  let start=false;
+  window.addEventListener('keydown', onKeyPress); 
+  function onKeyPress(event) {
+      switch (event.key) {
+          case ' ': 
+              start = true;
+              break;
+          default:
+              //console.log(`Key ${event.key} pressed`);
+  
+      }
+  }
 //   const spaceShipTexture = new THREE.TextureLoader().load('../assets/FuturisticCoating3.jpg');
 //   spaceShipTexture.wrapS = THREE.RepeatWrapping;
 //   spaceShipTexture.wrapT = THREE.RepeatWrapping;
@@ -48,79 +61,28 @@ export function createScene2Pt5(renderer, camera) {
     const mesh= gltf.scene;
     Spaceship=mesh;
     //Spaceship = new gltf.scene;
-    Spaceship.scale.set(0.5,0.5,0.5);
-    Spaceship.position.set(-8, -2.2, -26);
-    Spaceship.rotation.set(3.14/8, -3.14/2, 0);
+    Spaceship.scale.set(0.3,0.3,0.3);
+    Spaceship.position.set(-8,-2.2, -22)
+    Spaceship.rotation.set(0, -3.14/2, 0);
     //console.log(Spaceship.matrix);
     scene.add(Spaceship);
 
     // directionalLight.target = Spaceship;
     // scene.add(directionalLight.target);
   });
+  let detectorGeometry = new THREE.CylinderGeometry(2, 2, .8, 32);
+  const detectorMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+   const detector = new THREE.Mesh(detectorGeometry, detectorMaterial);
+   detector.position.set(-8, .7, -18.1);
+   scene.add(detector);
+   detector.visible=false;
 
+   let detector2Geometry = new THREE.BoxGeometry(2,.8,2);
+   const detector2 = new THREE.Mesh(detector2Geometry, detectorMaterial);
+   detector2.position.set(-6, .7, -18.1);
+   scene.add(detector2);
+   detector2.visible=false;
   
-
-  //let rocket = Spaceship;
-  //scene.add(rocket);
-
-  
-  // angle the rocket
-  //rocket.rotateZ(-0.8);
-
-//   let noseCone = new THREE.Mesh( new THREE.ConeGeometry(0.8, 1.5, 128), new THREE.MeshStandardMaterial({ map: spaceShipTexture, roughness: 0.3, metalness: 1.0 })); //color: 0xffffff }));
-//   const noseConeHeight = 2.25;
-//   noseCone.position.set(0, noseConeHeight, 0);
-
-//   const boosterEngineWidth = 0.8;
-//   let boosterEngine = new THREE.Mesh( new THREE.CylinderGeometry(boosterEngineWidth, 0.8, 3), new THREE.MeshStandardMaterial({ map: spaceShipTexture, roughness: 0.3, metalness: 1.0 }));//THREE.MeshBasicMaterial({ color: 0xffffff }));
-//   boosterEngine.position.set(-8, -2.2, 0);
-
-//   const wingStabilityVertices = new Float32Array([
-//   0,  2.2, 0, // Top
-//   -1.8, -1.5, 0, // lower left
-//   1.8, -1.5, 0  // lower right
-//  ]);
-
-//   const wingsGeoBuffer = new THREE.BufferGeometry();
-//   wingsGeoBuffer.setAttribute('position', new THREE.BufferAttribute(wingStabilityVertices, 3));
-  
-//   //stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ map: spaceShipTexture, side: THREE.DoubleSide }));
-//   let stabilityWings = new THREE.Mesh(wingsGeoBuffer, new THREE.MeshBasicMaterial({ color: 0x4293f5, side: THREE.DoubleSide }));
-
-//   const uvMapping = new Float32Array([
-//     0.5, 1, // top
-//     0, 0, // down left
-//     1, 0 // down right
-//   ]);
-
-//   wingsGeoBuffer.setAttribute('uv', new THREE.BufferAttribute(uvMapping, 2));
-
-//   boosterEngine.material.map = spaceShipTexture;
-//   noseCone.material.map = spaceShipTexture;
-//   stabilityWings.material.map = spaceShipTexture;
-
-//   // add the flame to the boosterEngine
-//   const flameTexture = new THREE.TextureLoader().load('../assets/flame7.jpg');
-//   flameTexture.wrapS = THREE.RepeatWrapping;
-//   flameTexture.wrapT = THREE.RepeatWrapping;
-
-//   const flame = new THREE.ConeGeometry(0.7, 2, 32);
-//   const flameMaterial = new THREE.MeshBasicMaterial({ map: flameTexture, transparent: true, opacity: 0.7 });
-//   const flameMesh = new THREE.Mesh(flame, flameMaterial);
-//   flameMesh.position.set(0, -2.2, 0);
-//   flameMesh.rotation.x = Math.PI; // rotate the flame to point downwards
-  
-//   // assemble the spaceship...  
-//   boosterEngine.add(noseCone);
-//   boosterEngine.add(flameMesh);
-//   boosterEngine.add(stabilityWings);
-
-
-  // boosterEngine combo alias:
-  //const rocket = boosterEngine;
-  //scene.add(rocket);
-  //rocket.position.set(0, 0, 0);//-10, -40);
-  //rocket.scale.set(0.35, 0.35, 0.35);
 
   window.addEventListener("keydown", function(event) {
    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "].includes(event.key)) {
@@ -130,16 +92,6 @@ export function createScene2Pt5(renderer, camera) {
 
  let timer = new THREE.Clock();
 
-
-//  function updateFlameEffect(elapsedTime) {
-//   // controls the flicker of the flame
-//   flameMesh.material.opacity = 1.0 - Math.sin(elapsedTime * 50)/6;
-//   // controls the rate of the up and down flame movement
-//   const scaleY = 1.5 + 0.3 * Math.sin(elapsedTime * 100);
-//   flameMesh.scale.y = scaleY;
-//   flameMesh.position.y = -2.2 - (scaleY - 1);
-//   flameMesh.rotation.y = flameMesh.rotation.y + 3; // rotate the flame for added realism
-//  }
 
   // add a series of pipe and ring obstacles that the rocket must navigate through
   const gameScale = 0.35;
@@ -203,7 +155,7 @@ export function createScene2Pt5(renderer, camera) {
       lowerPipe.add(lowerPipeCap);
       
   
-      let ring = new THREE.Mesh(new THREE.TorusGeometry(2, 0.5, 16, 100), new THREE.MeshStandardMaterial({ map: pipeTexture, roughness: 0.3, metalness: 1.0 }));
+      let ring = new THREE.Mesh(new THREE.TorusGeometry(6, 0.5, 16, 100), new THREE.MeshStandardMaterial({ map: pipeTexture, roughness: 0.3, metalness: 1.0 }));
       let ringSide = LOW_SIDE;
       if (Math.random() > 0.5)
       {
@@ -245,12 +197,15 @@ export function createScene2Pt5(renderer, camera) {
 
   let velocityY = 0;
   let gameOverAlerted = false;
+  alert("Mission: Get through the minefield without having two collisions! Press the space bar to go! ");
 
   function animate()
   {
     requestAnimationFrame(animate);
-
-    if (!gameComplete && !gameOverAlerted)
+    //Spaceship.updateMatrixWorld();
+    
+    //console.log(start);
+    if (!gameComplete && !gameOverAlerted&& start)
     {
         // Get the elapsed time
         let elapsedTime = timer.getElapsedTime();
@@ -262,6 +217,8 @@ export function createScene2Pt5(renderer, camera) {
         if (Spaceship)
         {
             Spaceship.position.y += velocityY - 0.13;
+            detector.position.y += velocityY - 0.13;
+            detector2.position.y += velocityY - 0.13;
         }
         
         // the pipes and rings will be moving from right to left while the rocket navigates through
@@ -329,26 +286,10 @@ export function createScene2Pt5(renderer, camera) {
 
     }
 
-    
-    //camera.position.z = -200;
-    //camera.position.set(0, 0, -200);
-    // if (Spaceship)
-    // {
-    //     camera.lookAt(Spaceship.position);
-    // }
-    //OrbitControls.length(100);
 
     renderer.render(scene, camera);
  }
  animate();
-
-//  function gameOver()
-//  {
-//   // while (scene.children.length > 0)
-//   // {
-//   //   scene.remove(scene.children[0]);
-//   // }
-//  }
 
 
   const redPlanetTexture = new THREE.TextureLoader().load('../assets/orange.png');
@@ -404,7 +345,7 @@ export function createScene2Pt5(renderer, camera) {
   const distantStars = createDistantStarField();
   scene.add(distantStars);
 
-
+ 
    // Event listeners for movement using arrow keys
   document.addEventListener('keydown', (event) => {
     if (!gameComplete)
@@ -434,48 +375,85 @@ export function createScene2Pt5(renderer, camera) {
   });
 
 let collisionTime = 0;
+//FUNCTIONS for collsion
+function isCylinderThroughRing(detector, ring) {
+    const distanceX = Math.abs(detector.position.x - ring.position.x);
+    const ringInnerRadius = ring.geometry.parameters.radius - ring.geometry.parameters.tube;
+    if (distanceX <= ringInnerRadius-(2)) {//2 for cylender radius
+      //console.log(`Spaceship went through ring`);
+      return true;
+    }else{
+      return false;
+    }
+}
+function isBoxThroughRing(detector, ring) {
+    const distanceX = Math.abs(detector.position.x - ring.position.x);
+    const ringInnerRadius = ring.geometry.parameters.radius - ring.geometry.parameters.tube;
+    if (distanceX <= ringInnerRadius-(1)) {//1 for half the distange from center of box
+      //console.log(`Spaceship went through ring`);
+      return true;
+    }else{
+      return false;
+    }
+  
+}
 
-   // Function to check for collisions
+function getBoundingBox(mesh) {
+  const box = new THREE.Box3().setFromObject(mesh);
+  return box;
+}
+function detectCollision(detector, pipes) {
+  const detectorBox = getBoundingBox(detector);
+  for (let i = 0; i < pipes.length; i++) {
+      const upperPipeBox = getBoundingBox(upperPipes[i]);
+      const lowerPipeBox = getBoundingBox(lowerPipes[i]);
+      if (detectorBox.intersectsBox(upperPipeBox) || detectorBox.intersectsBox(lowerPipeBox)) {
+          console.log(`Collision detected with pipe pair ${i}!`);
+          return true;
+      }
+  }
+  return false;
+}
+
+function checkCollisionWithToruses(mesh, rings) {
+  const shipAABB = getBoundingBox(mesh);
+
+  for (let i = 0; i < rings.length; i++) {
+      const ringAABB = getBoundingBox(rings[i]);
+
+      if (shipAABB.intersectsBox(ringAABB)&&!isCylinderThroughRing(detector,rings[i])&&!isBoxThroughRing(detector2,rings[i])) {
+          console.log(`Collision detected with ring ${i}!`);
+          return true; 
+      }
+  }
+
+  return false; 
+}
+
+
   function checkCollision()
   {
     if (!Spaceship)
     {
         return;
     }
-
+//THIS WAS ADDED: basically uses the functions I made with AABB bounding box mesh for upper and lower pipes and the mesh object I custom made because the GLTF object center was really off
     let collisionDetected = false;
-
-    const collisionDistance = 1.5;
-    for (let i = 0; i < upperPipes.length; ++i)
-    {
-      let distanceX = Math.abs(Spaceship.position.x - upperPipes[i].position.x);
-      let distanceY = Math.abs(Spaceship.position.y - upperPipes[i].position.y);
-      if (distanceX < collisionDistance && Spaceship.position.y > (upperPipes[i].position.y - 72)) //distanceY < (collisionDistance - 2 )) 
-      {
-        collisionDetected = true;
-        break;
-      }
-      distanceX = Math.abs(Spaceship.position.x - lowerPipes[i].position.x);
-      distanceY = Math.abs(Spaceship.position.y - lowerPipes[i].position.y);
-      if (distanceX < collisionDistance && Spaceship.position.y < (lowerPipes[i].position.y + 70)) //distanceY < (collisionDistance - 2))
-      {
-        collisionDetected = true;
-        break;
-      }
-      distanceX = Math.abs(Spaceship.position.x - rings[i].position.x);
-      distanceY = Math.abs(Spaceship.position.y - rings[i].position.y);
-      if (distanceX < 2.0 && distanceY < 1)
-      {
-        collisionDetected = true;
-        break;
-      }
-
+    if(detectCollision(detector,upperPipes)||detectCollision(detector2,upperPipes)){
+      collisionDetected=true;
     }
+    //ALSO ADDED: uses bounded box and also checks if the object is going through the ring
+    if(checkCollisionWithToruses(detector2,rings)||checkCollisionWithToruses(detector,rings)){
+      collisionDetected=true;
+    }
+    
+
+
+    
     
      if (collisionDetected) {
       console.log("Collision detected!");
       // rocket turns red on collision
-
       if (collisionTime == 0.0)
       {
         ++collisionCounter;
@@ -486,10 +464,6 @@ let collisionTime = 0;
       {
         ++collisionCounter;
       }        
-
-      //Spaceship.material.color.set(0xff0000);
-      //noseCone.material.color.set(0xff0000);
-      //stabilityWings.material.color.set(0xff0000);
 
       if (Spaceship)
       {
@@ -508,10 +482,6 @@ let collisionTime = 0;
         collisionTime = 0.0;
       }
     } else {
-      // reset back to normal color
-      //Spaceship.material.color.set(0xffffff);
-      //noseCone.material.color.set(0xffffff);
-      //stabilityWings.material.color.set(0x4293f5);
       if (Spaceship)
         {
            Spaceship.traverse((child) => {
